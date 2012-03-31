980eef0555b7e7623a00cfea9c133bcd71d23f23
Florian Ragwitz <rafl@debian.org>
2012-03-31 09:54:26 +0200

    Refactor the watcher

--- lib/Glob/Watcher.pm
+++ lib/Glob/Watcher.pm
@@ -1,45 +1,77 @@
+package Glob::Watcher;
 
-sub run_watcher {
-    my $watcher_pid = $$;
+use Moose;
+use syntax 'method', 'function';
+use Proc::Fork;
+use MooseX::Types::Moose 'CodeRef';
+use MooseX::Types::Common::Numeric 'PositiveInt';
+use aliased 'Parallel::Runner';
+use namespace::autoclean;
 
-    run_fork {
-        child {
-            try {
-                $0 = 'walker';
-                run_walker();
-            }
-            catch {
-                die "walked exited: $_";
-            }
-            finally {
-                #kill SIGTERM, $watcher_pid;
-            };
+has max_unpacker_processes => (
+    is       => 'ro',
+    isa      => PositiveInt,
+    required => 1,
+    default  => 16,
+);
 
-            exit 0;
-        }
-        parent {
-            $0 = 'watcher';
+has fork_manager => (
+    is      => 'ro',
+    isa     => Runner,
+    lazy    => 1,
+    builder => '_build_fork_manager',
+    handles => {
+        run_child         => 'run',
+        running_children  => 'children',
+        wait_for_children => 'finish',
+    },
+);
 
-            my $pm = Parallel::Prefork->new({
-                max_workers => 16,
-            });
+has workload => (
+    is       => 'ro',
+    isa      => CodeRef,
+    required => 1,
+);
 
-            while ($pm->signal_received ne 'TERM') {
-                $pm->start(sub {
-                    warn "spawning new worker";
-                    try {
-                        $0 = 'unpacker';
-                        run_unpacker();
-                    }
-                    catch {
-                        die "worker exited abnormally: $_";
-                    };
-                });
-            }
+method _build_fork_manager {
+    my $runner = Runner->new( $self->max_unpacker_processes );
+    $runner->reap_callback(fun ($status, $pid) {
+        warn "worker $pid exited with $status";
+        $self->run_many( $self->workload );
+    });
 
-            $pm->wait_all_children;
-            warn "all done";
-            exit 0;
-        }
-    };
+    return $runner;
 }
+
+method BUILD {
+    $self->fork_manager;
+}
+
+method run_many ($cb) {
+    my $needed_children = $self->max_unpacker_processes - $self->running_children;
+
+    $self->run_child($cb) for 1 .. $needed_children;
+}
+
+method run {
+    my $watcher_pid = $$;
+    $0 = 'watcher';
+
+    $self->run_many(fun {
+        warn "spawning new worker";
+        try {
+            $0 = $self->workload_name;
+            $self->workload->();
+        }
+        catch {
+            die "worker exited abnormally: $_";
+        };
+    });
+
+    $self->wait_for_children;
+    warn "all done";
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
